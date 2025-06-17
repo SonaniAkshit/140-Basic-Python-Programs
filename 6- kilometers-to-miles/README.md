d38fe928d9c3518f34373d1a5fac558416852ce7
SonaniAkshit <sonaniakshit684@gmail.com>
2025-06-17 11:50:09 +0530

    Add program 6: convert kilometers to miles in Python
    This program takes input in kilometers and converts it to miles
    using the conversion factor (1 km = 0.621371 miles).

---
+++ 6- kilometers-to-miles/README.md
@@ -0,0 +1,8 @@
+# Program 6
+ Write a Python program to convert kilometers to miles.
+
+###### Output
+ ```python
+Enter distance in kilometers: 100
+100.0 kilometers = 62.137100000000004 miles
+```
---
+++ 6- kilometers-to-miles/main.py
@@ -0,0 +1,9 @@
+kilometer = float(input("Enter distance in kilometers: "))
+
+# Conversion factor : 1 kilometer = 0.621371 miles
+
+conversion_factor = 0.621371
+
+miles = kilometer * conversion_factor
+
+print(f"{kilometer} kilometers = {miles} miles")
